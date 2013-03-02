68238dcab1cdb605bc6a4801ab9b6f750c8f90ae
David Roncancio <kuryaki@Davids-MacBook-Air.local>
2013-03-01 23:19:34 -0500

    moving site to github

---
+++ js/tabs.js
@@ -0,0 +1,17 @@
+
+    function tabSwitch(active, number, tab_prefix, content_prefix) {
+
+        
+        for (var i=1; i < number+1; i++) {
+
+                document.getElementById(content_prefix+i).style.display = 'none';
+
+                document.getElementById(tab_prefix+i).className = 'tab';
+
+        }
+
+            document.getElementById(content_prefix+active).style.display = 'block';
+
+            document.getElementById(tab_prefix+active).className = 'tab-active';
+
+        }
