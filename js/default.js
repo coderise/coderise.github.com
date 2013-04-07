34193fd4f18d5de3bf9519ac54eb79bbfa5f86e6
Elizabeth R <eramirem@gmail.com>
2013-04-07 19:51:59 -0400

    Responsive site

---
+++ js/default.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.dropdown').on('click', function() {
+		$(this).find('ul').toggle('slow');
+	});
+
+	$('.icon-list').on('click', function() {
+		$('nav[role="navigation"] > ul').toggle('slow');
+	});
+});
