3887a3e75494d9ea2b954cb0898bd65386f8865c
Elizabeth R <eramirem@gmail.com>
2013-04-11 08:45:54 -0400

    Menu animation slow and hide others than $(this)

--- js/default.js
+++ js/default.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	$('.dropdown').on('click', function() {
-		$(this).find('ul').toggle('slow');
+		$(this).find('ul').toggle('fast');
+		$(this).siblings().find('ul').hide();
 	});
 
 	$('.icon-list').on('click', function() {
-		$('nav[role="navigation"] > ul').toggle('slow');
+		$('nav[role="navigation"] > ul').toggle('fast');
 	});
 
 	
