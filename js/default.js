bf45d4b70457031f3674ba48fed50a7c390df446
Josue Daniel Bustamante <josue@coderise.org>
2015-03-24 17:47:56 -0500

    Update the Index, Team and 404 pages
    + In Index add the section for the places who Coderise are, edita and
    add the logos of supports.
    + In Team edit the members and change the style who see, and add the
    social networks icons.
    + In 404 only fixed the positions.
    + Create a new icons files.
    + Edit the images folder order
    - Remove the Students section
    = Change Spanish web first
    + Add the English li for nav
    + Fix an error at the index.html es file

--- js/default.js
+++ js/default.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
 	$('.icon-list').on('click', function() {
 		$('nav[role="navigation"] > ul').toggle('fast');
 	});
-
+	
+	$('.dropdownPlaces').on('click',function(){
+		if($('.down').css('display')!='none'){
+			$('.up').html('&#9650;').show().siblings('span').hide();
+		} else if ($('.up').css('display')!='none'){
+			$('.down').show().siblings('span').hide();
+		}
+	});
 	
 });
